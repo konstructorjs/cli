43b6e758bc092e504c95131417a3c88ac0e5eaaa
Manthan Mallikarjun <nahtnam@gmail.com>
2017-09-03 18:34:36 -0700

    feat(secret): add secret command

--- src/scripts/secret.js
+++ src/scripts/secret.js
@@ -1,1 +1,19 @@
 
+const chalk = require('chalk');
+const uuidV4 = require('uuid/v4');
+
+const secret = async () => {
+  const key = uuidV4();
+  console.log();
+  console.log(key);
+  console.log();
+};
+
+module.exports.command = 'secret';
+module.exports.desc = 'generate a secret key';
+
+module.exports.handler = () => {
+  secret().catch((err) => {
+    console.log(chalk.red(err));
+  });
+};
